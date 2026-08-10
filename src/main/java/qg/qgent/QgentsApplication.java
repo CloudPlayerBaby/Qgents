f280bf352323156b845fccd3f3bdd3fd24ff83f6
Hiiii113 <3322858298@qq.com>
2026-08-10 17:21:37 +0800

    refactor: reorganize project structure

--- src/main/java/qg/qgent/QgentsApplication.java
+++ src/main/java/qg/qgent/QgentsApplication.java
@@ -1,13 +1,13 @@
-package qg.qgent.demo;
+package qg.qgent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class QgentsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(QgentsApplication.class, args);
     }
 
 }
